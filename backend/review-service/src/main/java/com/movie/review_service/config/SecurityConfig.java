630277eb06503fe177e5e124665998b3bfbf63aa
Soumi12-codeX <dsoumi192@gmail.com>
2026-03-20 16:47:46 +0530

    fixed cors erros

--- backend/review-service/src/main/java/com/movie/review_service/config/SecurityConfig.java
+++ backend/review-service/src/main/java/com/movie/review_service/config/SecurityConfig.java
@@ -47,7 +47,8 @@ public class SecurityConfig {
 
         config.setAllowedOrigins(List.of(
             "http://localhost:5173",
-            "http://localhost:3000"
+            "http://localhost:3000",
+            "https://movie-matrix-gamma.vercel.app"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
